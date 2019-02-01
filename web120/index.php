4e7ad6794298bc6d156f9b7f1b1d03dc180e3321
cagunning <46728544+cagunning@users.noreply.github.com>
2019-02-01 14:01:26 -0800

    Add files via upload
    does not include config.php file because it contains re-CAPTCHA credentials.

---
+++ web120/index.php
@@ -0,0 +1,28 @@
+<?php include 'includes/config.php';?>
+<?php include 'includes/header.php';?>
+<!-- end header file here -->         
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Welcome</h2>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.jpg" class="desktop" alt="" />
+ <img src="images/phone.jpg" class="phone" alt="" />
+ <br>
+ <p>Hi, my name is Christian, this is my second quarter at seattle central.</p>
+ <p>I am studying web development and liking it so far.</p>
+ <p>Not sure what else to really say about me other than that.</p>
+ <p>I did move from california 5 years ago, and have still not adjusted to the weather.</p>
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>Right Column</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="" />
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+</aside>
+<!-- END RIGHT COL -->
+<?php include 'includes/footer.php';?>
+
